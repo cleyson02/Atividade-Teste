af9dfa5cdcb35084db372d1990dbaaf5f523bb71
Cleyson Silva de Oliveira <cleysonsilva2002@gmail.com>
2025-09-05 23:23:14 -0300

    add projeto angular front end

---
+++ frontend/frontend/src/app/bottom-sheet/bottom-sheet.ts
@@ -0,0 +1,11 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-bottom-sheet',
+  imports: [],
+  templateUrl: './bottom-sheet.html',
+  styleUrl: './bottom-sheet.css'
+})
+export class BottomSheet {
+
+}
